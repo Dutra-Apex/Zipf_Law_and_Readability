27dd26bc1419c3c969516460b6b86fc9351699a3
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2020-05-01 03:12:37 -0400

    Update zipf.py

--- zipf.py
+++ zipf.py
@@ -4,12 +4,12 @@ import numpy as np
 import textatistic
 
 # Prints the Zipfian distribution of a text
-def count(text):
-    file = open(text, "rt", encoding="UTF-8")
-    #file = open(text, "rt")
+def zipf(text):
+    file = open(text, "rt", encoding="UTF-8") # Opens file with proper encoding
     text = (file.read()).lower()
     file.close()
 
+    # Replaces any character that can interfer with the word frequency distribution
     text = text.replace(",", "")
     text = text.replace(".", "")
     text = text.replace("!", "")
@@ -20,23 +20,22 @@ def count(text):
     text = text.replace("—", "")
     text = text.replace(";", "")
     text = text.replace("\n", "")
-
-
-    ########################################################################################################
-
+    
+    # Splits the text into words, putting them into a list
     words = text.split()
     word_count = 0
     dict_w = {}
 
+    # Loops through the list, counts their frequency and  creates a dictionary (dict_w) that contains each word with their number of occurences
     for i in range(len(words)):
         for j in words:
             if j == words[i]:
                 word_count += 1
-
         dict_w[words[i]] = word_count
         word_count = 0
         i += 1
 
+    # Sorts the dictionary from the most frequent word to the least frequent
     sorted_dict = sorted(dict_w.items(), reverse=True, key=lambda x: x[1])
     word_list = []
     count_list = []
@@ -47,37 +46,18 @@ def count(text):
 
     rank_list = []
 
+    # Creates a list containing the log of the word's occurance and rank
     for i in range(0, len(count_list)):
         count_list[i] = math.log(count_list[i], 10)
         rank_list.append(math.log(i+1, 10))
 
+    # Initializes the log of the list of ranks and the log of the list of frequencies as numpy arrays for future use
     rank_list = np.array(rank_list)
     count_list = np.array(count_list)
-    #########################################################################################################
 
-    char_count = 0
-    char_count_list = []
-    alphabet = []
-    char = []
 
-    for word in text:
-        char += word.split()
-
-    for i in range(97,123):
-        alphabet.append(chr(i))
-
-    for i in range(len(alphabet)):
-        for k in range(len(char)):
-            if alphabet[i] == char[k]:
-                char_count += 1
-
-        char_count_list.insert(i, char_count)
-        char_count = 0
-
-    #########################################################################################################
 
     #Equations for line of best fit
-
     denominator = rank_list.dot(rank_list) - rank_list.mean() * rank_list.sum()
     m = (rank_list.dot(count_list) - count_list.mean() * rank_list.sum()) / denominator
     b = (count_list.mean() * rank_list.dot(rank_list) - rank_list.mean() * rank_list.dot(count_list)) / denominator
@@ -86,8 +66,9 @@ def count(text):
     residual = count_list - best_fit
     total = count_list - count_list.mean()
     r_squared = 1 - residual.dot(residual) / total.dot(total)
+    
+    
     print("The R^2 is: ", r_squared)
-
     print("The slope is: ", m)
 
 
@@ -114,13 +95,13 @@ def count(text):
 # Function that prints the readability score of a given text
 def reading_ease(txt):
     file = open(txt, "rt", encoding="UTF-8")
-    #file = open(txt, "rt")
     text = (file.read()).lower()
     file.close()
     print()
     print(txt)
-    print("The readability score is: ",textatistic.flesch_score(text))
+    print("The readability score is: ",textatistic.flesch_score(text))  #Uses the library textatistic, which contains dictionaries from PyHyphen
 
 
-count("sample.txt")
+# Call functions by using any sample text encoded in UTF-8
+zipf("sample.txt")
 reading_ease("sample.txt")
